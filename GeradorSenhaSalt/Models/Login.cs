36035252177224effa1dd256cc4479bc1a9b9a67
André Henrique <andre.henrique0207@gmail.com>
2023-08-07 21:05:20 -0300

    Git add

---
+++ GeradorSenhaSalt/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace EscritorioVirtual.Shared.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Username { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Salt { get; set; }
+    }
+
+}
---
+++ GeradorSenhaSalt/Utils/LoginUtils.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace EscritorioVirtual.Api.Utils
+{
+    public class LoginUtils
+    {
+        public static string GerarHash(string senha, byte[] salt)
+        {
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: senha!,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+
+        public static string ToString(byte[] salt)
+        {
+            return string.Join(',', salt);
+        }
+
+        public static byte[] ToByte(string salt)
+        {
+            var strs = salt.Split(',');
+            var bytes = new byte[strs.Length];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(strs[i]);
+            }
+            return bytes;
+        }
+
+        public static byte[] GerarSalt()
+        {
+            return RandomNumberGenerator.GetBytes(128 / 8);
+        }
+    }
+}
